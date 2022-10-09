e4c1cb6af1c5f186ec4293598fd0848f7ef6ed54
Aryan Kumar <aryan.ak434@gmail.com>
2022-10-09 14:25:26 +0530

    Add error handling to folder creator

--- links/generator.py
+++ links/generator.py
@@ -11,14 +11,10 @@ def generateCLI() -> None:
     title = str(input("Title: "))
     link = str(input("Link: "))
     document = t.template(title, link)
-    os.mkdir("links/" + title)
-    with open("links/" + title + "/index.html", "w") as f:
-        f.write(document)
-    commit("Added " + title)
-
-def generate(title = "Ba3a", link = "https://ba3a.me") -> None:
-    document = t.template(title, link)
-    os.mkdir("links/" + title)
+    try:
+        os.mkdir("links/" + title)
+    except FileExistsError:
+        print("Link already exists. Overwriting...")
     with open("links/" + title + "/index.html", "w") as f:
         f.write(document)
     commit("Added " + title)
